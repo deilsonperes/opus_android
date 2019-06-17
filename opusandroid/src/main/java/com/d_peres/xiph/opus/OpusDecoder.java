0d99235a3a11f7f1e67659d1b93d1eb5883c8a55
Deilson Peres <deilson.peres@gmail.com>
2019-06-16 23:05:31 -0300

    Added basic documentation

--- opusandroid/src/main/java/com/d_peres/xiph/opus/OpusDecoder.java
+++ opusandroid/src/main/java/com/d_peres/xiph/opus/OpusDecoder.java
@@ -19,18 +19,38 @@ public class OpusDecoder {
 	private native int native_decode(byte[] in, int num_samples, short[] out);
 	private native void native_destroy();
 	
+	/**
+	 * Creates an Opus Decoder.<br>
+	 * The sampling rate and must be 8000, 12000, 16000, 24000, or 48000
+	 * The numner of channels can be either 1 or 2 (mono/stereo)
+	 *
+	 * @param sampleRate the sample rate
+	 * @param numChannels the number of channels
+	 */
 	public OpusDecoder(
 			@SampleRate int sampleRate,
 			@Channels int numChannels) {
 		OpusError.throwOnError(native_init(sampleRate, numChannels));
 	}
 	
+	/**
+	 * Decodes an opus frame.
+	 *
+	 * @param in the input signal (compressed opus bytes)
+	 * @param num_samples the number of bytes in the input signal
+	 * @param out the pcm output signal
+	 * @return the number of samples decoded (per channel)
+	 */
 	public int decode(byte[] in, int num_samples, short[] out) {
 		int ret;
 		OpusError.throwOnError(ret = native_decode(in, num_samples, out));
 		return ret;
 	}
 	
+	/**
+	 * Frees the native memory allocated for this decoder. After this is called
+	 * the decoder can no longer be used.
+	 */
 	public void destroy() {
 		native_destroy();
 	}
